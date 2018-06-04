a593b63c09c070af9f5562f6083a4e641fd70ed4
Beata Markunas <beata.markunas@gmail.com>
2018-06-05 00:16:17 +0200

    Create a basic Title component.

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const Title = () => {
+    return (
+        <div className="title">
+            <h1>Sudoku Game</h1>
+        </div>
+    )
+}
+
+export default Title;
