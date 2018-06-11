5259f94a6c7f8ba19ed65f97f35c11af39dc9d83
Beata Markunas <beata.markunas@gmail.com>
2018-06-11 08:07:11 +0200

    Improve styles.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,27 +5,29 @@ import styles from './Navigation.css';
 const Navigation = (props) => {
     return (
         <div className={styles.Navigation}>
-        <div className="nav">
-            <button onClick={() => props.newGameHandler()}>New Game</button>
-            <button onClick={() => props.restartNewGame()}>Restart</button>
-            <button onClick={() => props.solveSudoku()}>Solve</button>
-            <button onClick={() => props.checkSudoku()}>Check</button>
-            {
-                props.error ? <div><p className="error-msg">{props.error}</p></div> : null
-            }
-            {
-                props.newGameClicked ? 
-                <div>
-                    <h1>Select level:</h1>
-                    <button onClick={(e) => props.startNewGame(e.target.textContent)}>easy</button>
-                    <button onClick={(e) => props.startNewGame(e.target.textContent)}>medium</button>
-                    <button onClick={(e) => props.startNewGame(e.target.textContent)}>hard</button>
-                    <button onClick={(e) => props.startNewGame(e.target.textContent)}>very-hard</button>
-                    <button onClick={(e) => props.startNewGame(e.target.textContent)}>insane</button>
-                    <button onClick={(e) => props.startNewGame(e.target.textContent)}>inhuman</button>
-                </div> : null
-            }
-        </div>
+            <div className="nav">
+                <button onClick={() => props.newGameHandler()}>New Game</button>
+                <button onClick={() => props.restartNewGame()}>Restart</button>
+                <button onClick={() => props.solveSudoku()}>Solve</button>
+                <button onClick={() => props.checkSudoku()}>Check</button>
+                
+                {
+                    props.error ? <div><p className="error-msg">{props.error}</p></div> : null
+                }
+                
+                {
+                    props.newGameClicked ? 
+                    <div>
+                        <h1>Select level:</h1>
+                        <button onClick={(e) => props.startNewGame(e.target.textContent)}>Easy</button>
+                        <button onClick={(e) => props.startNewGame(e.target.textContent)}>Medium</button>
+                        <button onClick={(e) => props.startNewGame(e.target.textContent)}>Hard</button>
+                        <button onClick={(e) => props.startNewGame(e.target.textContent)}>Very-hard</button>
+                        <button onClick={(e) => props.startNewGame(e.target.textContent)}>Insane</button>
+                        <button onClick={(e) => props.startNewGame(e.target.textContent)}>Inhuman</button>
+                    </div> : null
+                }
+            </div>
         </div>
     )
 }
